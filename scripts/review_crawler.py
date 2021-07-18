228d4f3fc727e5b723f9a671b43294337824fa82
Miguel Mano <miguelmanofernandes@gmail.com>
2021-07-18 23:08:34 +0100

    Initial commit

---
+++ scripts/review_crawler.py
@@ -0,0 +1,96 @@
+import os
+import json
+import requests
+import dataclasses
+from pathlib import Path
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+from dataclasses import dataclass, field
+
+@dataclass
+class Game:
+    appid: int = -1
+    name: str = ''
+    reviews: list[str] = field(default_factory=list)
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://steamdb.info/graph/')
+
+driver.find_element_by_xpath("//select[@name='table-apps_length']/option[text()='25']").click()
+appids = [app.get_attribute('data-appid') for app in driver.find_elements_by_class_name('app')]
+
+db = []
+
+Path('banners/').mkdir(parents=True, exist_ok=True)
+
+for i, appid in enumerate(appids):
+    game = Game()
+
+    # Navigate to the reviews page, sorted by funny
+    driver.get(f'https://steamcommunity.com/app/{appid}/reviews/?browsefilter=funny')
+
+    # Do not crawl non-store apps, like Source SDK and Spacewar
+    if driver.current_url == 'https://store.steampowered.com/':
+        continue
+
+    # Download banner image
+    with open(f'./banners/{appid}_header.jpg', 'wb') as file:
+        res = requests.get(f'https://cdn.akamai.steamstatic.com/steam/apps/{appid}/header.jpg')
+        file.write(res.content)
+
+    # Assign appid
+    game.appid = appid
+    
+    try:
+        # Age check might prompt, try to continue
+        driver.find_element_by_id('age_gate_btn_continue').click()
+        driver.find_element_by_xpath("//select[@name='ageYear']/option[text()='1900']").click()
+    except NoSuchElementException:
+        pass
+
+    try:
+        driver.find_element_by_xpath("//select[@name='ageYear']/option[text()='1900']").click()
+        driver.find_element_by_css_selector('.btnv6_blue_hoverfade[href="#"]').click()
+    except NoSuchElementException:
+        pass
+
+    # Find game name based on appid
+    try:
+        game.name = driver.find_element_by_class_name('apphub_AppName').text
+    except NoSuchElementException:
+        continue
+
+    # Delete a couple of elements so I can easily extract review text
+    driver.execute_script("""
+        let el1 = document.getElementsByClassName('date_posted');
+        while (el1[0]) el1[0].parentNode.removeChild(el1[0]);
+
+        let el2 = document.getElementsByClassName('early_access_review');
+        while (el2[0]) el2[0].parentNode.removeChild(el2[0]);
+
+        let el3 = document.getElementsByClassName('received_compensation');
+        while (el3[0]) el3[0].parentNode.removeChild(el3[0]);
+
+        let el4 = document.getElementsByClassName('refunded');
+        while (el4[0]) el4[0].parentNode.removeChild(el4[0]);
+    """)
+
+    reviews = [review.text for review in driver.find_elements_by_class_name('apphub_CardTextContent')]
+
+    # Only keep reviews that are tweet sized
+    game.reviews = [review for review in reviews if len(review) < 280]
+
+    if len(game.reviews) > 0:
+        db.append(dataclasses.asdict(game))
+
+    # Print progress to console
+    os.system('cls||clear')
+    print(f'[{i+1}/{len(appids)}] {game.name} ({game.appid})')
+
+driver.close()
+
+with open('reviews.json', 'w') as file:
+    json_str = json.dumps(db, indent=2)
+    file.write(json_str)
---
+++ src/App.svelte
@@ -0,0 +1,166 @@
+<script lang="ts">
+	import * as _ from 'lodash';
+	import * as db from '../models/reviews.json';
+
+	type App = {
+		appid: number;
+		name: string;
+		reviews: string[];
+	};
+
+	const getRandomApps = (n: number): App[] => {
+		return _.sampleSize(Object.values(db['default']), n);
+	}
+
+	const getRandomReview = (app: App) : string => {
+		// const regex = new RegExp(app.name, 'gi');
+		// const spoiler = [...Array(Math.floor(Math.random() * (20 - 10 + 1)) + 10)].map(x => '*').join('');
+		// return _.sample(app.reviews).replaceAll(regex, `<span class="spoiler">${spoiler}</span>`);
+
+		return _.sample(app.reviews);
+	}
+
+	const onGuess = (guess: App) => {
+		if (guess.appid === correct.appid) {
+			score++;
+			new Audio('sfx/correct.mp3').play();
+		}
+		else {
+			score = 0;
+			new Audio('sfx/wrong.mp3').play();
+		}
+
+		apps = getRandomApps(3);
+		correct = _.sample(apps);
+	}
+
+	let score = 0;
+	let apps: App[] = getRandomApps(3);
+	let correct: App = _.sample(apps);
+</script>
+
+<main>
+	<h1>it's ok, i guess</h1>
+	<h4>Guess from which game the Steam review came from!</h4>
+
+	<div class="score-container">
+		<div class="score-bubble">
+			{ score }
+		</div>
+	</div>
+
+	<div class="choice-container">
+		{#each apps as app}
+			<div class="choice" on:click={() => onGuess(app)}>
+				<img src="banners/{app.appid}_header.jpg" alt="" />
+				<span>{ app.name }</span>
+			</div>
+		{/each}
+	</div>
+
+	<div class="review-container">
+		<span class="big-quotation-mark">"</span>
+		<span class="review-body">{ @html getRandomReview(correct) }</span>
+	</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		max-width: 1200px;
+		margin: 0 auto;
+		height: 100%;
+	}
+
+	h1 {
+		font-size: 5em;
+		font-weight: 900;
+		margin: 1.25em 0 0 0;
+	}
+
+	h4 {
+		font-weight: normal;
+		margin-bottom: 4em;
+	}
+
+	.score-container {
+		display: flex;
+		justify-content: center;
+	}
+
+	.score-bubble {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 75px;
+		height: 75px;
+		background: #ffffff0f;
+		border-radius: 50%;
+		font-size: 32px;
+	}
+
+	.choice-container {
+		display: flex;
+		justify-content: space-around;
+		margin: 1em 0;
+	}
+
+	.choice {
+		display: flex;
+		flex-direction: column;
+		width: 400px;
+		height: 225px;
+		background: #ffffff0f;
+		border-radius: 10px;
+		overflow: hidden;
+		transition: all .2s ease;
+		cursor: pointer;
+	}
+
+	.choice:first-child {
+		margin-right: 1em;
+	}
+
+	.choice:last-child {
+		margin-left: 1em;
+	}
+
+	.choice:hover {
+		background: whitesmoke;
+		color: #1f2126;
+	}
+
+	.choice > img {
+		max-width: 100%;
+		max-height: 100%;
+	}
+
+	.choice > span {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-grow: 1;
+	}
+
+	.review-container {
+		display: flex;
+		flex-direction: column;
+		text-align: left;
+		width: 100%;
+		font-family: 'Motiva Sans Italic';
+		height: 200px;
+	}
+
+	.big-quotation-mark {
+		font-size: 128px;
+		height: 40px;
+		color: #ffffff49;
+	}
+
+	.review-body {
+		margin: 0 0 2.5em 2.5em;
+		font-size: 28px;
+	}
+</style>
